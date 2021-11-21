8dab38a85e3cdbe1fe54ac5f10a8ef16eff6cc70
VtotheK <ville.keituri1@gmail.com>
2021-11-21 13:20:32 -0800

    removed single control bits, replaced with array

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,42 +1,53 @@
 `include "adder.v"
 
 module ALU( 
-    input wire [16:0] x,
-    input wire [16:0] y,
-    input wire zx,
-    input wire nx,
-    input wire zy,
-    input wire ny,
-    input wire f,
-    input wire no,
-    output reg [16:0] out,
+    input wire [15:0] x,
+    input wire [15:0] y,
+    input wire [5:0] op,
+    output reg [15:0] out,
     output reg zr,
     output reg ng
 );
 
 wire co;
+reg [15:0] temp;
 always @(*)
-    begin
-        if(zx) begin
-            out = 16'b0;
-        end
-        if(zy) begin
-            out = 16'b0;
-        end
-        if(nx) begin
-            out = -x;
-        end
-        if(ny) begin
-            out = -y;
-        end
-        if(f) begin
-            out = x + y;
-        end
-        else if(!f) begin
-            out = x&y;
-        end
-        if(no) begin
-            out = !out; 
-        end
-end
+    
+    if(op == 6'b101010) begin //0
+    end
+    else if(op == 6'b111111) begin //1
+    end
+    else if(op == 6'b111010) begin //-1
+    end
+    else if(op == 6'b001100) begin //x
+    end
+    else if(op == 6'b110000) begin //y
+    end
+    else if(op == 6'b001101) begin //!x
+    end
+    else if(op == 6'b110001) begin //!y
+    end
+    else if(op == 6'b001111) begin //-x
+    end
+    else if(op == 6'b110011) begin //-y
+    end
+    else if(op == 6'b110011) begin //x+1
+    end
+    else if(op == 6'b110111) begin //y+1
+    end
+    else if(op == 6'b001110) begin //x-1
+    end
+    else if(op == 6'b110010) begin //y-1
+    end
+    else if(op == 6'b000010) begin //x+y
+    end
+    else if(op == 6'b010011) begin //x-y
+    end
+    else if(op == 6'b000111) begin //y-x
+    end
+    else if(op == 6'b000000) begin //x&y
+    end
+    else if(op == 6'b010101) begin //x|y
+    end
+
 endmodule
--- ALU/test.v
+++ ALU/test.v
@@ -1,15 +1,10 @@
 `include "alu.v"
 
 module alu_tb(
-    output reg [16:0] x,
-    output reg [16:0] y,
-    output reg zx,
-    output reg nx,
-    output reg zy,
-    output reg ny,
-    output reg f,
-    output reg no,
-    input wire signed[16:0] result,
+    output reg [15:0] x,
+    output reg [15:0] y,
+    output reg [5:0] op,
+    input wire signed[15:0] result,
     input wire zr,
     input wire ng
 );
@@ -19,16 +14,30 @@ integer i;
 initial begin
     x = 1;
     y = 1;
-    $display("x\ty\tzx\tnx\tzy\tny\tf\tno\tresult\tzr\tng\t");
-    {zx, nx, zy, ny, f, no} = 6'b101010; 
-    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",x,y,zx,nx,zy,ny,f,no,result,zr,ng);
-    #100;
-    {zx, nx, zy, ny, f, no} = 6'b000001; 
-    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",x,y,zx,nx,zy,ny,f,no,result,zr,ng);
-    #100;
+    $display("zx\tnx\tzy\tny\tf\tno\tresult\tzr\tng\t");
+    op  = 6'b101010; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
+    #10;
+    op  = 6'b111111; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
+    #10;
+    op = 6'b111010; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
+    #10;
+    op  = 6'b001100; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
+    #10;
+    op  = 6'b001100; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
+    #10;
+    op = 6'b110000; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
+    #10;
+    op  = 6'b001101; 
+    $monitor("%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t%0d\t",op[0],op[1],op[2],op[3],op[4],op[5],result,zr,ng);
     #10;
 end
 
-ALU derp(x,y,zx,nx,zy,ny,f,no,result,zr,ng);
+ALU derp(x,y,op,result,zr,ng);
 
 endmodule
