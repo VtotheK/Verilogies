d59e47ba7d9ac51fd88d3b1e6df224d39c2b9db3
VtotheK <ville.keituri1@gmail.com>
2021-11-25 00:46:15 +0200

    ran out of test cases

--- ALU/alu.v
+++ ALU/alu.v
@@ -15,64 +15,86 @@ reg [15:0] zero = 16'b0;
 reg [15:0] one = 16'b1;
 reg [15:0] xval;
 reg [15:0] yval;
+wire [15:0] value;
 
 adder sum(xval,yval,value,co);
 
-
-always @(*)
-    if(op == 6'b010101) begin //0
+always @(*) if(op == 6'b010101) begin //0
+        out = value;
         xval = 16'b0;
         yval = 16'b0;
     end
     else if(op == 6'b111111) begin //1
+        out = value;
         xval = 16'b1;
         yval = 16'b0;
     end
     else if(op == 6'b010111) begin //-1
+        out = value;
         xval = 16'b0;
         yval = ~16'b0;
     end
     else if(op == 6'b001100) begin //x
+        out = value;
         xval = x;
         yval = 16'b0;
     end
     else if(op == 6'b000011) begin //y
-        yval = y;
+        out = value;
         xval = 16'b0;
+        yval = y;
     end
     else if(op == 6'b101100) begin //!x
+        out = value;
         xval = !x;
         yval = 16'b0;
     end
     else if(op == 6'b110001) begin //!y
-        yval = !y;
+        out = value;
         xval = 16'b0;
+        yval = !y;
     end
     else if(op == 6'b111100) begin //-x
-        yval = !y;
-        xval = 16'b0;
+        out = value;
+        xval = -x;
+        yval = 16'b0;
     end
-    /*
     else if(op == 6'b110011) begin //-y
-        out = -y;
+        out = value;
+        xval = 16'b0;
+        yval = -y;
     end
     else if(op == 6'b111110) begin //x+1
-        out = increment_x;
+        out = value;
+        xval = x;
+        yval = 16'b1;
     end
-    else if(op == 6'b110111) begin //y+1
-        out = increment_y;
+    else if(op == 6'b111011) begin //y+1
+        out = value;
+        xval = 16'b1;
+        yval = y;
     end
     else if(op == 6'b011100) begin //x-1
-        out = decrement_x;
+        out = value;
+        xval = x;
+        yval = ~(16'b0);
     end
     else if(op == 6'b010011) begin //y-1
-        out = decrement_y;
+        out = value;
+        xval = ~(16'b0);
+        yval = y;
     end
     else if(op == 6'b010000) begin //x+y
-        out = sum_xy;
+        out = value;
+        xval = x;
+        yval = y;
     end
     else if(op == 6'b010011) begin //x-y
+        out = value;
+        xval = x;
+        yval = -y;
     end
+    /*
     else if(op == 6'b000111) begin //y-x
     end
     else if(op == 6'b000000) begin //x&y
